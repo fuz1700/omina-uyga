e38b3bcd4eb7f1b459605d29ffd79d7dfc446929
fuz1700 <fuz77it@gmail.com>
2024-10-31 16:43:54 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+let nav = document.querySelector(".nav")
+
+function navChange() {
+    nav.style.backgroundColor = "red"
+  
+}
+
+let aside = document.querySelector(".aside")
+
+function asideChange() {
+    aside.style.backgroundColor = "yellow"
+  
+}
+
+
+
+let main = document.querySelector(".main")
+
+function mainChange() {
+    main.style.backgroundColor = "black"
+  
+}
+
+
+
+let footer = document.querySelector(".footer")
+
+function footerChange() {
+    footer.style.backgroundColor = "pink"
+  
+}
